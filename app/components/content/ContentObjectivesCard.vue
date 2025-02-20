8a5777d5160428d0c1774f55cb8668889df6c55b
swhawes <shawes@fiu.edu>
2025-02-20 16:46:57 -0500

    LMM temp update

---
+++ app/components/content/ContentObjectivesCard.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  objectives: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+console.log("Received Objectives:", props.objectives);
+</script>
+
+<template>
+  <div
+    class="p-4 border-2 border-green-500 bg-gray-700 text-white rounded-lg shadow-lg"
+  >
+    <h2 class="text-lg font-bold text-green-400">Objectives</h2>
+    <ul class="list-disc pl-5">
+      <li v-for="(objective, index) in objectives" :key="index">
+        <strong>{{ objective.action }}:</strong> {{ objective.description }}
+      </li>
+    </ul>
+  </div>
+</template>
